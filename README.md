6db38d59765a2d26dcdb45c7923bdf381ce588a3
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-11-17 19:27:37 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# logger
-A simple Node.Js logger module with configurable options.
+# Logger Module
+A simple <a href="" target=""><strong>Node.Js</strong></a> module with configurable options.
+
+# Installation
+```bash
+npm install logger
+```
+
+# Usage
+```js
+const Logger = require('logger');
+
+const options = {
+  logFilePath: 'my-log-file.log'
+};
+
+const logger = new Logger(options);
+
+logger.logError('An error has occurred.');
+logger.logInfo('Important information.');
+logger.logSucces('Succes message.')
+```
+
+# Options
+Initialize the Logger with the following options:
+<ul>
+    `logFilePath` (optional): The path to the log file. Default is 'default-log-file.log'.
+</ul>
+
+# Methods
+<ul>
+    <li>`logError` - Logs an error message to the configured log file.</li>
+    <li>`logSucces` - Logs an succes message to the configured log file.</li>
+    <li>`logInfo` - Logs an info message to the configured log file.</li>
+</ul>
---
+++ logger.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+
+class Logger {
+    constructor(options = {}) {
+        this.logFilePath = options.logFilePath || 'default-log-file.log';
+
+        if (!fs.existsSync(this.logFilePath)) {
+            fs.writeFileSync(this.logFilePath, '', { flag: 'wx' });
+        }
+    }
+
+    logError(errorMessage) {
+        const logMessage = this.formatLogMessage('ERROR', errorMessage);
+        this.appendToLogFile(logMessage);
+    }
+
+    logInfo(infoMessage) {
+        const logMessage = this.formatLogMessage('INFO', infoMessage);
+        this.appendToLogFile(logMessage);
+    }
+
+    logSucces(infoMessage) {
+        const logMessage = this.formatLogMessage('SUCCES', infoMessage);
+        this.appendToLogFile(logMessage);
+    }
+
+    appendToLogFile(logMessage) {
+        fs.appendFile(this.logFilePath, logMessage, (err) => {
+            if (err) {
+                console.error('Error writing to log file:', err);
+            }
+        });
+    }
+
+    formatLogMessage(logLevel, message) {
+        const formattedDate = this.getCurrentDateTime();
+        return `[${logLevel}] ${formattedDate} ${message}\n`;
+    }
+
+    getCurrentDateTime() {
+        const now = new Date();
+        return now.toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    }
+}
+
+module.exports = Logger;
